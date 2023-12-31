3657fd211fa8fb59c2b494cb4a3c2f7219dcf41b
Julio Santos <julioc.9730@gmail.com>
2023-12-31 14:58:36 -0300

    Update app_steps.rb

--- features/step_definitions/app_steps.rb
+++ features/step_definitions/app_steps.rb
@@ -1,9 +1,10 @@
 Given('the access user home') do
   @app_screens = App_Screens.new
+  @app_screens.open_app
 end
 
 When('he selects an option show value') do
-
+  @app_screens.select_show_balance
 end 
 
 Then('displays its value') do
