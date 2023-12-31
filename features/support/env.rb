1a3e2077f144354e5de0c61403a82cd057c504b1
Julio Santos <julioc.9730@gmail.com>
2023-12-31 15:37:19 -0300

    Update env.rb

--- features/support/env.rb
+++ features/support/env.rb
@@ -2,7 +2,3 @@ require 'cucumber'
 require 'rspec'
 require 'rufo'
 require 'appium_lib'
-
-caps = Appium.load_appium_txt file: File.expand_path("caps_appium/android.txt", __dir__), verbose: true
-Appium::Driver.new(caps, true)
-Appium.promote_appium_methods Object
