7aae1c7dfdafda3d6ab569e2183bdf13e359c83c
Julio Santos <julioc.9730@gmail.com>
2023-12-31 14:45:46 -0300

    Create app_screens.rb

---
+++ features/screens/app_screens.rb
@@ -0,0 +1,13 @@
+class App_Screens
+  def open_app
+    find_element(:xpath, '//*[contains(@text=, "NuConta")]').displayed?
+  end
+
+  def select_show_balance
+    find_element(:accessibility_id, 'show-balance').click 
+  end
+
+  def value_user_balance
+    return find_element(:accessibility_id, 'user-balance').text
+  end
+end
