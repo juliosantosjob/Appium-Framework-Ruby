04b59e92ae722461db62a7ccedf4a227c589fb44
Julio Santos <julioc.9730@gmail.com>
2023-12-31 15:40:04 -0300

    Create hooks.rb

---
+++ features/support/hooks.rb
@@ -0,0 +1,8 @@
+Before do 
+  caps = Appium.load_appium_txt file: File.expand_path('caps_appium/android.txt', __dir__), verbose: true
+  Appium::Driver.new(caps, true)
+  Appium.promote_appium_methods Object
+
+  @driver.start_driver
+  @driver.set_wait(20)
+end 
