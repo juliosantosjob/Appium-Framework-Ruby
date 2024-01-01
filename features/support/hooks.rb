c9f5b86f13ad824695e40df71036fbe2521ac4f5
Julio Santos <julioc.9730@gmail.com>
2024-01-01 20:21:28 -0300

    Update hooks.rb

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -7,12 +7,12 @@ Before do
       Appium::Driver.new(caps, true)
       Appium.promote_appium_methods Object
     else
-    caps = Appium.load_appium_txt file: File.expand_path('caps_appium/android.txt', __dir__), verbose: true
+      caps = Appium.load_appium_txt file: File.expand_path('caps_appium/android.txt', __dir__), verbose: true
       Appium::Driver.new(caps, true)
       Appium.promote_appium_methods Object
    rescue Exception => e
-    puts e.message
-    Process.exit(0)
+      puts e.message
+      Process.exit(0)
 end
 
   @driver.start_driver
