2cdc5bf44e59315565caca1212c5e583254c899d
Julio Santos <julioc.9730@gmail.com>
2024-01-01 20:12:33 -0300

    Update hooks.rb

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,4 +1,8 @@
 Before do 
+
+  cloud = ENV['CLOUD']
+
+  if (cloud == true) 
   caps = Appium.load_appium_txt file: File.expand_path('caps_appium/android.txt', __dir__), verbose: true
   Appium::Driver.new(caps, true)
   Appium.promote_appium_methods Object
