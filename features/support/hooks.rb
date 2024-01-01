042bcfdeb3ca185b3f5f4ddba048e2cb45fa927f
Julio Santos <julioc.9730@gmail.com>
2024-01-01 20:20:39 -0300

    Update hooks.rb

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,11 +1,19 @@
 Before do 
-
   cloud = ENV['CLOUD']
 
-  if (cloud == true) 
-  caps = Appium.load_appium_txt file: File.expand_path('caps_appium/android.txt', __dir__), verbose: true
-  Appium::Driver.new(caps, true)
-  Appium.promote_appium_methods Object
+  begin
+    if (cloud == 'true') 
+      caps = Appium.load_appium_txt file: File.expand_path('caps_appium/android.txt', __dir__), verbose: true
+      Appium::Driver.new(caps, true)
+      Appium.promote_appium_methods Object
+    else
+    caps = Appium.load_appium_txt file: File.expand_path('caps_appium/android.txt', __dir__), verbose: true
+      Appium::Driver.new(caps, true)
+      Appium.promote_appium_methods Object
+   rescue Exception => e
+    puts e.message
+    Process.exit(0)
+end
 
   @driver.start_driver
   @driver.set_wait(DEFAULT_HOLD)
