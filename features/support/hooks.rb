bba4c835aa8edeb0c4b3433a9429b7be29c906a8
Julio Santos <julioc.9730@gmail.com>
2024-01-02 17:51:15 -0300

    Update hooks.rb

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,27 +1,24 @@
 Before do
-  caps = Appium.load_appium_txt file: File.join(__dir__,'caps', 'android_appium.txt')
-  Appium::Driver.new(caps, true)
-  Appium.promote_appium_methods Object
-
   @driver.start_driver
   @driver.set_wait(DEFAULT_HOLD)
+  @driver.manage.timeouts.implicit_wait = 15
 end
 
-AfterStep('@screenshot') do |scenario|
-  binary_shot = driver.screenshot_as(:base64)
-  temp_shot = 'output/temp_shot.png'
+After do |scenario| # rubocop:disable Lint/UnusedBlockArgument
+  # binary_shot = driver.screenshot_as(:png)
+  shot = @driver.save_screenshot('output/evidence.png')
 
-  File.open(temp_shot, 'wb') do |f|
-    f.write(Base64.decode64(binary_shot).force_encoding('UTF-8'))
-  end
+  # File.open('output/evidence.png', 'wb') do |f|
+  #   f.write(Base64.decode64(binary_shot).force_encoding('UTF-8'))
+  # end
 
   Allure.add_attachment(
     name: 'screenshot',
     type: Allure::ContentType::PNG,
-    source: File.open(temp_shot)
+    source: File.open(shot)
   )
 end
 
 After do
-  driver.driver_quit
+  @driver.driver_quit
 end
