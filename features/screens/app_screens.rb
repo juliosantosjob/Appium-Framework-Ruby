12f08f05108736d3eca71c6e70e6853635463076
Julio Santos <julioc.9730@gmail.com>
2024-01-02 14:18:52 -0300

    Update app_screens.rb

--- features/screens/app_screens.rb
+++ features/screens/app_screens.rb
@@ -1,18 +1,14 @@
 class App_Screens
-  
+
   def open_app
-    find_element(:xpath, '//*[contains(@text=, "NuConta")]').displayed?
+    find_element(:xpath, '//*[contains(@text,"NuConta")]').displayed?
   end
 
   def select_show_balance
-    find_element(:accessibility_id, 'show-balance').click 
+    find_element(:accessibility_id, 'show-balance').click
   end
 
   def value_user_balance
     return find_element(:accessibility_id, 'user-balance').text
-  end
-
-  def balance_displayed
-    return find_element(:accessibility_id, 'user-balance')
   end
 end
