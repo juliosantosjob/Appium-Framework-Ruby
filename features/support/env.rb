d45f72481a219790b2e5afc335216120e2498ddd
Julio Santos <julioc.9730@gmail.com>
2024-01-02 14:20:06 -0300

    Update env.rb

--- features/support/env.rb
+++ features/support/env.rb
@@ -2,5 +2,15 @@ require 'cucumber'
 require 'rspec'
 require 'rubocop'
 require 'appium_lib'
+require 'yaml'
+require 'allure-cucumber'
 
 DEFAULT_HOLD = 20
+
+FileUtils.rm_f(Dir.glob('output/*.json'))
+FileUtils.rm_f(Dir.glob('output/*.png'))
+
+Allure.configure do |config|
+  config.results_directory = 'output/allure-results'
+  config.clean_results_directory = true
+end
