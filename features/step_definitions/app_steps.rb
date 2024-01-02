4b1dfab3a3a0691a79a8f6d7ed2fa7ccdd6fec36
Julio Santos <julioc.9730@gmail.com>
2024-01-02 14:19:08 -0300

    Update app_steps.rb

--- features/step_definitions/app_steps.rb
+++ features/step_definitions/app_steps.rb
@@ -5,18 +5,9 @@ end
 
 When('he selects an option show value') do
   @app_screens.select_show_balance
-end 
+end
 
 Then('displays its value: {string}') do |value|
   get_balance = @app_screens.value_user_balance
   expect(get_balance).to eq(value)
-end
-
-And('the value is displayed') do
-  @app_screens.balance_displayed
-end
-
-Then('hide the value: {string}') do |string|
-  get_hide_balance = @app_screens.balance_displayed
-  expect(get_hide_balance.displayed?).to be_falsey
 end
