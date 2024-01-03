2ba2b29a33abbc6e6af88b9e1e3e03d721918052
Julio Santos <julioc.9730@gmail.com>
2024-01-03 11:39:59 -0300

    Update hooks.rb

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,24 +1,18 @@
 Before do
-  @driver.start_driver
-  @driver.set_wait(DEFAULT_HOLD)
-  @driver.manage.timeouts.implicit_wait = 15
-end
-
-After do
-  binary_shot = @driver.screenshot_as(:base64)
-  temp_shot = 'logs/temp_shot.png'
-
-  File.open(temp_shot, 'wb') do |f|
-    f.write(Base64.decode64(binary_shot).force_encoding('UTF-8'))
-  end
-
-  Allure.add_attachment(
-    name: 'screenshot',
-    type: Allure::ContentType::PNG,
-    source: File.open(temp_shot)
-  )
+  driver.start_driver
+  driver.set_wait(DEFAULT_HOLD)
 end
 
 After do
   driver.driver_quit
+end
+
+After do
+  shot = screenshot('output/temp_evidence.png')
+
+  Allure.add_attachment(
+    name: 'Screenshot',
+    type: Allure::ContentType::PNG,
+    source: File.open(shot)
+  )
 end
