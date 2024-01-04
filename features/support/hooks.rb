55bcf0f56decb6da6cc9b7984e7f8be5d6c8c63b
Julio <julio958214@gmail.com>
2024-01-04 11:11:46 -0300

    ajusts

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,3 +1,51 @@
+# Before do
+#   driver.start_driver
+# end
+
+# After do
+#   driver.driver_quit
+# end
+
+# Before do
+#   driver.set_wait(DEFAULT_HOLD)
+#   output_directory = "output"
+
+#   if Dir.exist?(output_directory)
+#     FileUtils.rm_f(Dir.glob("#{output_directory}/*"))
+#   else
+#     Dir.mkdir(output_directory)
+#   end
+# end
+
+# After do
+#   begin
+#   binary_shot = driver.screenshot_as(:base64)
+#   temp_shot = "output/temp_shot.png"
+
+#   File.open(temp_shot, "wb") do |f|
+#     f.write(Base64.decode64(binary_shot).force_encoding("UTF-8"))
+#   end
+
+#   rescue => exception
+#     puts "Error: Unable to perform screenshot action! #{exception.message}"
+#   end
+
+#   Allure.add_attachment(
+#     name: "evidence.png",
+#     type: Allure::ContentType::PNG,
+#     source: File.open(temp_shot),
+#     test_case: true
+#   )
+# end
+
+
+
+
+
+
+
+
+
 Before do
   driver.start_driver
 end
@@ -8,30 +56,25 @@ end
 
 Before do
   driver.set_wait(DEFAULT_HOLD)
-  if Dir.exist?('output')
-    FileUtils.rm_f(Dir.glob('output/*'))
+  output_directory = "output"
+
+  if Dir.exist?(output_directory)
+    FileUtils.rm_f(Dir.glob("#{output_directory}/*"))
   else
-    Dir.mkdir 'output'
+    Dir.mkdir(output_directory)
   end
 end
 
 After do
   begin
-  binary_shot = driver.screenshot_as(:base64)
-  temp_shot = 'output/temp_shot.png'
-
-  File.open(temp_shot, "wb") do |f|
-    f.write(Base64.decode64(binary_shot).force_encoding("UTF-8"))
-  end
-
+    @shot = screenshot('output/evidence.png')
   rescue => exception
     puts "Error: Unable to perform screenshot action! #{exception.message}"
   end
 
   Allure.add_attachment(
-    name: 'evidence.png',
+    name: 'screenshot',
     type: Allure::ContentType::PNG,
-    source: File.open(temp_shot),
-    test_case: true
+    source: File.open(@shot)
   )
 end
