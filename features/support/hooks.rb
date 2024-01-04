3def0cbe7cffdb8e748c911680a444c4e814a062
Julio <julio958214@gmail.com>
2024-01-04 11:43:57 -0300

    ajusts

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,51 +1,3 @@
-# Before do
-#   driver.start_driver
-# end
-
-# After do
-#   driver.driver_quit
-# end
-
-# Before do
-#   driver.set_wait(DEFAULT_HOLD)
-#   output_directory = "output"
-
-#   if Dir.exist?(output_directory)
-#     FileUtils.rm_f(Dir.glob("#{output_directory}/*"))
-#   else
-#     Dir.mkdir(output_directory)
-#   end
-# end
-
-# After do
-#   begin
-#   binary_shot = driver.screenshot_as(:base64)
-#   temp_shot = "output/temp_shot.png"
-
-#   File.open(temp_shot, "wb") do |f|
-#     f.write(Base64.decode64(binary_shot).force_encoding("UTF-8"))
-#   end
-
-#   rescue => exception
-#     puts "Error: Unable to perform screenshot action! #{exception.message}"
-#   end
-
-#   Allure.add_attachment(
-#     name: "evidence.png",
-#     type: Allure::ContentType::PNG,
-#     source: File.open(temp_shot),
-#     test_case: true
-#   )
-# end
-
-
-
-
-
-
-
-
-
 Before do
   driver.start_driver
 end
@@ -56,25 +8,32 @@ end
 
 Before do
   driver.set_wait(DEFAULT_HOLD)
-  output_directory = "output"
 
+  output_directory = "output"
   if Dir.exist?(output_directory)
-    FileUtils.rm_f(Dir.glob("#{output_directory}/*"))
+    FileUtils.rm_rf("#{output_directory}/*")
   else
     Dir.mkdir(output_directory)
   end
 end
 
-After do
+After do |scenario|
   begin
-    @shot = screenshot('output/evidence.png')
-  rescue => exception
-    puts "Error: Unable to perform screenshot action! #{exception.message}"
+    binary_shot = driver.screenshot_as(:base64)
+    temp_shot_path = "output/#{scenario.name}_evd_#{scenario.failed? ? 'success' : 'error' }.png"
+
+    File.open(temp_shot_path , "wb") do |f|
+    f.write(Base64.decode64(binary_shot).force_encoding("UTF-8"))
+
+  rescue => e
+    puts "Error: Unable to perform screenshot action! #{e.message}"
   end
 
-  Allure.add_attachment(
-    name: 'screenshot',
-    type: Allure::ContentType::PNG,
-    source: File.open(@shot)
-  )
+    Allure.add_attachment(
+      name: temp_shot_path ,
+      type: Allure::ContentType::PNG,
+      source: File.open(temp_shot_path ),
+      test_case: true
+    )
+  end
 end
